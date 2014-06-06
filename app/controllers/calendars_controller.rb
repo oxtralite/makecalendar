b371cfaa09267ed60d71f81697f1c17cbb9737da
Joel Duffin <joel.duffin@gmail.com>
2014-06-06 02:43:47 -0600

    Render events in the timezone of the calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,11 +1,25 @@
 class CalendarsController < ApplicationController
 
+  around_filter :set_time_zone, :only => :show
+
   def index
     @calendars = Calendar.all.order(:title)
   end
 
   def show
-    @calendar = Calendar.find(params[:id])
+    set_calendar
+    @events = @calendar.events
+  end
+
+  def set_time_zone(&block)
+    set_calendar
+    Time.use_zone(@calendar.timezone, &block)
+  end
+
+  private
+
+  def set_calendar
+    @calendar ||= Calendar.find(params[:id])
   end
 
 end
